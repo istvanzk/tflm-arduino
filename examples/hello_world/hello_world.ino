419603afae47b14535f8cc1488a14745b0b9d146
Istvan Z. Kovacs <istvanzk@gmail.com>
2024-06-15 23:30:48 +0200

    Correct TensorFlowLiteMicro.h include

--- examples/hello_world/hello_world.ino
+++ examples/hello_world/hello_world.ino
@@ -15,7 +15,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-#include <TensorFlowLite.h>
+#include <TensorFlowLiteMicro.h>
 
 #include "constants.h"
 #include "main_functions.h"
